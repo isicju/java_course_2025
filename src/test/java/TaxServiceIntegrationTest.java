07c90a3dbd4f7377b444aff1c939333800927285
icju <isicju@gmail.com>
2025-09-22 21:14:26 +0300

    adding assert for integration test

--- src/test/java/TaxServiceIntegrationTest.java
+++ src/test/java/TaxServiceIntegrationTest.java
@@ -1,12 +1,15 @@
 import com.example.demo.TaxService;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class TaxServiceIntegrationTest {
 
     @Test
     public void simpleIntegrationTest(){
         System.out.println("Running simpleIntegrationTest test...");
         TaxService taxService = new TaxService();
+        assertNotNull(taxService);
     }
 
 }
