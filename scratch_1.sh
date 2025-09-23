7454b20310194d0fc6b98bc61e547081e6fb42c8
Vagin <vagin@skytecgames.com>
2025-09-23 17:00:00 +0500

    fix scripts

--- scratch_1.sh
+++ scratch_1.sh
@@ -7,7 +7,5 @@ sudo adduser app_user --disabled-password --gecos "app_user,,,"
 echo "app_user:password" | sudo chpasswd
 sudo usermod -aG sudo app_user
 find java_course_2025 -type d -exec chmod 755 {} +
-find java_course_2025 -type f -exec chmod 644 {} +
+find java_course_2025 -type f -exec chmod 744 {} +
 sudo chown -R app_user: java_course_2025
-
-su - app_user -c './scratch_build_and_run.sh'
