dc79ab445fee956a9918032e028b868ac21e5bf7
Glovluk <79121131077@yandex.ru>
2025-09-27 16:11:16 +0300

    домашка 1 - сервис - тесты

--- src/test/java/TaxServiceIntegrationTest.java
+++ src/test/java/TaxServiceIntegrationTest.java
@@ -1,15 +1,63 @@
 import com.example.demo.SimpleServer;
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import io.restassured.path.json.config.JsonPathConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.JsonConfig.jsonConfig;
+import static io.restassured.config.RestAssuredConfig.config;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TaxServiceIntegrationTest {
+
+    private static final int PORT = 8080;
+
+    @BeforeAll
+    static void setup() {
+        new SimpleServer().startServer(PORT);
+        RestAssured.port = PORT;
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.defaultParser = Parser.JSON;
+
+        RestAssured.config = config()
+                .jsonConfig(jsonConfig().numberReturnType(JsonPathConfig.NumberReturnType.DOUBLE));
+    }
+
+    @AfterAll
+    static void teardown(){
+        SimpleServer.stop();
+    }
 
     @Test
     public void simpleIntegrationTest(){
         System.out.println("Running simpleIntegrationTest test...");
         SimpleServer simpleServer = new SimpleServer();
         assertNotNull(simpleServer);
+    }
+
+    @Test
+    public void rootPath(){
+        given()
+                .when()
+                .get("/")
+                .then()
+                .statusCode(200)
+                .body(containsString("<html"));
+    }
+
+    @Test
+    public void taxPath(){
+        given()
+                .when()
+                .get("/tax")
+                .then()
+                .statusCode(200)
+                .body("size()", equalTo(15))
+                .body("[0].beforeIncome", closeTo(0.0, 1e-4));
     }
 
 }
--- src/test/java/TaxServiceUnitTest.java
+++ src/test/java/TaxServiceUnitTest.java
@@ -1,4 +1,3 @@
-import com.example.demo.SimpleServer;
 import com.example.demo.TaxService;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +10,31 @@ public class TaxServiceUnitTest {
         System.out.println("Running testEmptyValue test...");
         TaxService taxService = new TaxService();
         assertEquals(0, taxService.getNetMonthlySalary(0));
+    }
+
+    @Test
+    public void grossLowerPersonalAllowanceButHigherNILowerThreshold(){
+        double gross = 10000;
+        double expectedIncomeTax = 0.0;
+        double expectedIN = (gross - 9112.0) * 0.08;
+        double expectedNetMonthly = (gross - expectedIN - expectedIncomeTax) / 12;
+
+        double actual = new TaxService().getNetMonthlySalary(gross);
+
+        assertEquals(expectedNetMonthly, actual, 0.01);
+    }
+
+    @Test
+    public void rangeBasicHigher(){
+        double gross = 50270.0;
+        double taxable = gross - 12570.0;
+        double expectedIncomeTax = taxable * 0.2;
+        double expectedNI = (50270 - 9112.0) * 0.08;
+        double expectedNetMonthly = (gross - expectedIncomeTax - expectedNI) / 12.0;
+
+        double actual = new TaxService().getNetMonthlySalary(gross);
+
+        assertEquals(actual, expectedNetMonthly, 0.01);
     }
 
 }
